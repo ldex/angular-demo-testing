f3ea368175e4fe1c985ce96e85d2365507f6ae38
ldex <laurent.duveau+git@gmail.com>
2021-03-06 17:22:14 -0500

    Fix forms tests

--- src/app/common/login.component.spec.ts
+++ src/app/common/login.component.spec.ts
@@ -17,7 +17,7 @@ describe('Login Component', () => {
     let component: LoginComponent;
     let fixture: ComponentFixture<LoginComponent>;
     let submitElement: DebugElement;
-    let loginElement: DebugElement;
+    let userNameElement: DebugElement;
     let passwordElement: DebugElement;
     let authServiceSpy: Spy<AuthService>;
 
@@ -44,13 +44,13 @@ describe('Login Component', () => {
         // get test component from the fixture
         component = fixture.componentInstance;
 
-        loginElement = fixture.debugElement.query(By.css('#username'));
+        userNameElement = fixture.debugElement.query(By.css('#username'));
         passwordElement = fixture.debugElement.query(By.css('#password'));
         submitElement = fixture.debugElement.query(By.css('button'));
     });
 
     it('Empty username and password should disabled the submit button', () => {
-        loginElement.nativeElement.value = "";
+        userNameElement.nativeElement.value = "";
         passwordElement.nativeElement.value = "";
         fixture.detectChanges();
         expect(submitElement.nativeElement.disabled).toBeTruthy();
@@ -58,33 +58,48 @@ describe('Login Component', () => {
     });
 
     it('Empty username should disabled the submit button', () => {
-        loginElement.nativeElement.value = "";
+        userNameElement.nativeElement.value = "";
         passwordElement.nativeElement.value = "123456789";
         fixture.detectChanges();
         expect(submitElement.nativeElement.disabled).toBeTruthy();
     });
-    
+
     it('Empty password should disabled the submit button', () => {
-        loginElement.nativeElement.value = "John";
+        userNameElement.nativeElement.value = "John";
         passwordElement.nativeElement.value = "";
         fixture.detectChanges();
         expect(submitElement.nativeElement.disabled).toBeTruthy();
     });
 
+    it('Username validation', () => {
+        let username = component.loginForm.form.controls["username"];
+        expect(username.valid).toBeFalsy();
+        expect(component.loginForm.valid).toBeFalsy();
+        username.setValue("John");
+        expect(username.valid).toBeTruthy();
+    });
+
+    it('Password validation', () => {
+        let password = component.loginForm.form.controls["password"];
+        expect(password.valid).toBeFalsy();
+        expect(component.loginForm.valid).toBeFalsy();
+        password.setValue("123456789");
+        expect(password.valid).toBeTruthy();
+    });
+
     it('Entering valid username and password should call login', () => {
-        loginElement.nativeElement.value = "John";
-        passwordElement.nativeElement.value = "12345678";
-        fixture.detectChanges();
+        let username = component.loginForm.form.controls["username"];
+        let password = component.loginForm.form.controls["password"];
+
+        username.setValue("John");
+        password.setValue("123456789");
 
         expect(component.loginForm.valid).toBeTrue();
-        
+
         authServiceSpy.login.and.nextOneTimeWith(true);
 
-        // This sync emits the event and the subscribe callback gets executed above
-        submitElement.triggerEventHandler('click', null);
-        
-        fixture.detectChanges();
-       
+        component.loginForm.ngSubmit.emit(null);
+
         expect(authServiceSpy.login).toHaveBeenCalled();
     });
 });
